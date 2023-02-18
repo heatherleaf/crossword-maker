c5b4da48583a7cc96670ffb5700c9c061420eee2
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2023-02-18 20:20:54 +0100

    Move dictionaries to separate directory

--- default_dictionaries.js
+++ default_dictionaries.js
@@ -1,9 +1,9 @@
 
 var default_dictionaries = {
-    "Svenska: Nyheter + Wikipedia (768 kb)": "lexicon-sv-news+wiki.js",
-    "Svenska: Wikipedia-titlar (776 kb)": "lexicon-sv-wikititlar.js",
-    "Svenska: Bloggtexter (745 kb)": "lexicon-sv-bloggmix.js",
-    "Svenska: Twitter (746 kb)": "lexicon-sv-twitter.js",
-    "Svenska: Nyheter från 1800-talet (753 kb)": "lexicon-sv-kubhist.js",
-    "Engelska: OANC-korpusen (746 kb)": "lexicon-en-oanc.js",
+    "Svenska: Nyheter + Wikipedia (768 kb)": "dictionaries/lexicon-sv-news+wiki.js",
+    "Svenska: Wikipedia-titlar (776 kb)": "dictionaries/lexicon-sv-wikititlar.js",
+    "Svenska: Bloggtexter (745 kb)": "dictionaries/lexicon-sv-bloggmix.js",
+    "Svenska: Twitter (746 kb)": "dictionaries/lexicon-sv-twitter.js",
+    "Svenska: Nyheter från 1800-talet (753 kb)": "dictionaries/lexicon-sv-kubhist.js",
+    "Engelska: OANC-korpusen (746 kb)": "dictionaries/lexicon-en-oanc.js",
 };
