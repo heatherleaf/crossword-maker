f378e794ed8beba6b2f35b3de5386b0acdb8e65f
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2023-03-03 20:20:09 +0100

    English translations

---
+++ i18n-english.js
@@ -0,0 +1,71 @@
+
+var I18N = window.I18N || {};
+I18N.eng = {};
+
+I18N.eng.info = {
+    name: "English",
+    code: "Eng",
+    flag: "🇬🇧",
+};
+
+I18N.eng.elems = {
+    '#page-title': "Crossword maker",
+    '#main-heading': "Crossword maker",
+    '#acknowledgements-by': "by",
+    '#upload-button-text': "Upload your own lexicon",
+    '#button-reset': "Clear crossword",
+    '#hide-solution-text': "Hide solution",
+    '#button-help': "Help",
+    '#wordlist-heading': "Search results",
+    '#button-wordlist': "Shuffle / Insert",
+    '#horizontal-clues-heading': "Horizontal →",
+    '#vertical-clues-heading': "Vertical ↓",
+};
+
+I18N.eng.elemtitles = {
+    '.button-resize.grow': "Grow",
+    '.button-resize.shrink': "Shrink",
+    '#button-wordlist': "Shuffle words or add word",
+};
+
+I18N.eng.cssvars = {
+    '--click-and-edit-title': '"(Click and edit the crossword title)"',
+    '--click-and-edit-clue': '"(Click and edit the clue)"',
+};
+
+I18N.eng.strings = {
+    "ord)": "words)",
+    "[Inte inläst]": "[Not loaded]",
+
+    "Ett korsord måste ha minst två rader!": "A crossword must have at least two rows!",
+    "Ett korsord måste ha minst två kolumner!": "A crossword must have at least two columns!",
+    "Raden är inte tom! Ta bort alla bokstäver och block innan du kan ta bort den.": 
+        "The row is not empty! Remove all letters and blocks before you can remove it.",
+    "Kolumnen är inte tom! Ta bort alla bokstäver och block innan du kan ta bort den.":
+        "Kolumnen är inte tom! Ta bort alla bokstäver och block innan du kan ta bort den.",
+
+    "Vill du ersätta": "Do you want to replace",
+    "med": "with",
+    "Är du säker att du vill ta bort": "Are you sure you want to remove",
+    "och": "and",
+
+    "Är du säker att du vill radera korsordet?": "Are you sure you want to erase the crossword?",
+    "Slumpa nya ord": "Shuffle the word list",
+    "Lägg till i korsordet": "Add word to crossword",
+    "Lägg till": "Add",
+    "Inga ord passar": "No matching words",
+    "Vill du lägga till ett eget ord?": "Do you want to add your own word?",
+
+    "För många resultat, jag visar bara ett slumpmässigt urval.": "Too many results, showing a random selection.", 
+    "Filtrera genom att skriva bokstäver i sökrutan:": "Filter by writing in the search box:",
+    "Visar": "Showing",
+    "passande ord": "matching words",
+    "ord av": "words out of",
+    "passande": "matching",
+
+    "Markera en rad eller kolumn för att få förslag på ord,": "Select a row or column to get word suggestions,",
+    "klicka sedan på ett förslag för att lägga till det.":    "then click on a suggestion to add that words.",
+    "Dubbelklicka på en bokstav för att ta bort det ordet.":  "Doubleclick on a letter to remove that words.",
+    "Dubbelklicka på en tom ruta för att skapa ett block.":   "Doubleclick on an empty cell to create a block.",
+};
+
